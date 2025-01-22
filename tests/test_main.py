ae4b6af3b2f1cd8395cfac2495de5feed1083879
mayambasimao <s041maya@gmail.com>
2025-01-22 17:18:11 +0000

    start project

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from todolist.main import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange(organizacao)
+    response = client.get('/')  # Act (acao)
+
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {'message': 'Olá Mundo'}
