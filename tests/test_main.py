b44e4a9c96936b9273451026c224fb17f26648b5
mayambasimao <s041maya@gmail.com>
2025-02-03 00:59:36 +0000

    updating endpoints to use the real database

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from todolist.schema import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     # client = TestClient(app)  # Arrange(organizacao)
@@ -31,34 +33,34 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_get_user_by_id(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+# not yet
+def test_get_user_by_id(client, user):
     response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
+        'username': 'Teste',
+        'email': 'teste@test.com',
         'id': 1,
     }
 
 
-def test_get_user_by_id_should_return_not_found(client):
+# not yet
+def test_get_user_by_id_should_return_not_found(client, user):
     response = client.get('/users/4')
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -75,6 +77,33 @@ def test_update_user(client):
     }
 
 
+def test_update_integrity_error(client, user):
+    # create #Fausto" register
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    # changing the user.username of fixture to fausto
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email alredy exists'
+    }
+
+
 def test_update_user_shoud_return_not_found(client):
     response = client.put(
         '/users/4',
@@ -88,14 +117,14 @@ def test_update_user_shoud_return_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_shoud_return_not_found(client):
+def test_delete_user_shoud_return_not_found(client, user):
     response = client.delete('/users/4')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
